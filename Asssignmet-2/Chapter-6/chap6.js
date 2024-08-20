c3f22d004c21f1af0127c441ebd306db02a5c1fa
Muhammad Mubashir <mubashirbehlum606@gmail.com>
2024-08-20 14:25:07 +0500

    Add files via upload

---
+++ Asssignmet-2/Chapter-6/chap6.js
@@ -0,0 +1,28 @@
+//Q.1
+
+// // Initialize the variable
+// var a = 10;
+
+// // Display the result in the browser
+// document.write("Result:<br>");
+// document.write("The value of a is:<br> " + a);
+// // Perform ++a (pre-increment)
+// document.write("The value of ++a is: " + (++a) + "<br>");
+// document.write("Now the value of a is: " + a + "<br>");
+
+// // Perform a++ (post-increment)
+// document.write("The value of a++ is: " + (a++) + "<br>");
+// document.write("Now the value of a is: " + a + "<br>");
+
+// // Perform --a (pre-decrement)
+// document.write("The value of --a is: " + (--a) + "<br>");
+// document.write("Now the value of a is: " + a + "<br>");
+
+// // Perform a-- (post-decrement)
+// document.write("The value of a-- is: " + (a--) + "<br>");
+// document.write("Now the value of a is: " + a + "<br>");
+
+//Q.2
+var a = 2, b = 1;
+var result = --a - --b + ++b + b--;
+document.write("Result: " + result);
